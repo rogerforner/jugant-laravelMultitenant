8e4e8c6bbea21bf14f567af8c87af1560638854a
Roger Forner Fabre <sir.rogerforner@gmail.com>
2021-02-15 12:50:56 +0100

    Central domains

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,7 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'bar.saas.test',
+        'saas.test',
     ],
 
     /**
